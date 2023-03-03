0664eecaba511b94f752aca5b3157c746a29cd1b
LoganHall195 <99156058+LoganHall195@users.noreply.github.com>
2023-03-03 13:23:06 -0600

    Initial Upload

---
+++ js/newgame.js
@@ -0,0 +1,23 @@
+function popupEntry() {
+    let popupWindow = document.getElementById('beginGameEntry');
+    let charSelect = document.getElementById('classSelection');
+    charSelect.style.opacity = '0';
+    charSelect.style.visibility = 'hidden';
+    popupWindow.style.visibility = 'visible';
+    popupWindow.style.opacity = '1';
+  }
+  
+  function fadeAll() {
+    let content = document.getElementById('content');
+    let popupWindow = document.getElementById('beginGameEntry');
+    popupWindow.style.visibility = 'hidden';
+    popupWindow.style.opacity = '0';
+    content.style.opacity = '0';
+    content.style.visibility = 'hidden';
+  }
+
+  const el = document.querySelector("#module");
+  el.addEventListener("mousemove", (e) => {
+    el.style.backgroundPositionX = -0.005*e.offsetX + "px";
+    el.style.backgroundPositionY = -0.005*e.offsetY + "px";
+  });
