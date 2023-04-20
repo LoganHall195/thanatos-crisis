aa63f6d0b26d7983ca4f1db9fef51bbb11a24ff1
TonyShark1323 <antoniomartinez1323@gmail.com>
2023-04-19 19:50:13 -0500

    Modified Inventory System

--- js/ui.js
+++ js/ui.js
@@ -50,28 +50,68 @@ function gridSelect(id) {
 let itemList = [
   {
     name: 'Scrap',
-    description: 'Salvaged scrap from an ship. Can be sold for Thanite',
+    description: 'Salvaged scrap from an ship. \r\nCan be sold for Thanite',
     type: 'Sellable Item',
     image: 'scrap metal re.png',
   },
   {
-    name: 'Alien relic',
+    name: 'Alien Relic',
     description: 'A relic from a different race. Can be sold for Thanite',
     type: 'Sellable Item',
-    image: 'scrap metal re.png',
+    image: 'alien relic.png',
   },
   {
     name: 'Valuable Ores',
     description:
       'Rare ores sought highly throughout the galaxy. Can be sold for Thanite',
     type: 'Sellable Item',
+    image: 'valuable ores.png',
+  },
+  {
+    name: 'High Tech Weapon System',
+    description:
+      'A higher performance weapon system. If in inventory, player will deal extra damage in battle',
+    type: 'Ship Mod',
+    image: 'htws.png',
+  },
+  {
+    name: 'High Tech Shield System',
+    description:
+      'A higher performance shield system. If in inventory, player will have boosted shields in battle',
+    type: 'Ship Mod',
+    image: 'htss.png',
+  },
+  {
+    name: 'Ancient Star Map',
+    description:
+      'An ancient map of the galaxy. Shows location of treasure among the stars.',
+    type: 'Star Map',
+    image: 'scrap metal re.png',
+  },
+  {
+    name: 'Strange Substance',
+    description: 'A strange substance of unknown origin',
+    type: 'Quest Item',
+    image: 'scrap metal re.png',
+  },
+  {
+    name: 'Weapon Fragments',
+    description:
+      'Fragments of an ancient and powerful weapon, could come in handy if all fragments are retrieved',
+    type: 'Weapon',
     image: 'scrap metal re.png',
   },
   {
     name: 'Shield Recharge',
     description: 'Recharge your shields if they are depleted',
     type: 'Ship Mod',
-    image: 'shield recharge.png',
+    image: 'shield recharge re.png',
+  },
+  {
+    name: 'Hull Repair Nanites',
+    description: "Nanites programmed to repair damage to your ship's hull",
+    type: 'Weapon',
+    image: 'scrap metal re.png',
   },
 ];
 // Player Inventory
@@ -79,6 +119,42 @@ let inventory = [
   {
     name: 'Scrap',
     quantity: 1,
+  },
+  {
+    name: 'Alien Relic',
+    quantity: 1,
+  },
+  {
+    name: 'Valuable Ores',
+    quantity: 1,
+  },
+  {
+    name: 'High Tech Weapon System',
+    quantity: 1,
+  },
+  {
+    name: 'High Tech Shield System',
+    quantity: 5,
+  },
+  {
+    name: 'Ancient Star Map',
+    quantity: 1,
+  },
+  {
+    name: 'Strange Substance',
+    quantity: 1,
+  },
+  {
+    name: 'Weapon Fragments',
+    quantity: 1,
+  },
+  {
+    name: 'Shield Recharge',
+    quantity: 1,
+  },
+  {
+    name: 'Hull Repair Nanites',
+    quantity: 10,
   },
 ];
 // Populate Grid
@@ -90,7 +166,7 @@ function populateGrid(items, list) {
     const result = list.find((item) => item.name);
 
     const image = document.createElement('img');
-    image.src = './images/custom/items/' + result.image;
+    image.src = './images/custom/items/' + list[i].image;
     image.alt = item.name;
     image.style = 'width: 65px; margin-left: -30px; margin-top: -33px';
     gridItem.appendChild(image);
@@ -110,41 +186,43 @@ window.onload = function () {
   populateGrid(inventory, itemList);
 };
 
-function debug(command){
-  if (command=="asteroid"){
-      if (document.getElementById("minigameIframe").style.visibility=="hidden") {
-          document.getElementById("minigameIframe").style.visibility="visible"
-          document.getElementById("minigameIframe").src="minigames/asteroid.html"
-      }
-      else {
-          document.getElementById("minigameIframe").style.visibility="hidden"
-          document.getElementById("minigameIframe").src=""
-      }
-  } else if (command=="battle"){
-      if (document.getElementById("minigameIframe").style.visibility=="hidden") {
-          document.getElementById("minigameIframe").style.visibility="visible"
-          document.getElementById("minigameIframe").src="minigames/battle.html"
-      }
-      else {
-          document.getElementById("minigameIframe").style.visibility="hidden"
-          document.getElementById("minigameIframe").src=""
-      }
+function debug(command) {
+  if (command == 'asteroid') {
+    if (
+      document.getElementById('minigameIframe').style.visibility == 'hidden'
+    ) {
+      document.getElementById('minigameIframe').style.visibility = 'visible';
+      document.getElementById('minigameIframe').src = 'minigames/asteroid.html';
+    } else {
+      document.getElementById('minigameIframe').style.visibility = 'hidden';
+      document.getElementById('minigameIframe').src = '';
+    }
+  } else if (command == 'battle') {
+    if (
+      document.getElementById('minigameIframe').style.visibility == 'hidden'
+    ) {
+      document.getElementById('minigameIframe').style.visibility = 'visible';
+      document.getElementById('minigameIframe').src = 'minigames/battle.html';
+    } else {
+      document.getElementById('minigameIframe').style.visibility = 'hidden';
+      document.getElementById('minigameIframe').src = '';
+    }
   }
   var cmdDown = false;
 
-  document.body.addEventListener('keydown', function(event) {
-  var key = event.keyCode || event.charCode || 0;
-  if ([192].indexOf(key) !== -1) {
-    cmdDown = true;
-    document.getElementById("debug").style.visibility="visible";
-  }   
+  document.body.addEventListener('keydown', function (event) {
+    var key = event.keyCode || event.charCode || 0;
+    if ([192].indexOf(key) !== -1) {
+      cmdDown = true;
+      document.getElementById('debug').style.visibility = 'visible';
+    }
   });
 
-  document.body.addEventListener('keyup', function(event) {
-  var key = event.keyCode || event.charCode || 0;
-  if ([192].indexOf(key) !== -1) {
-    cmdDown = false;
-    document.getElementById("debug").style.visibility="hidden";
-  }
+  document.body.addEventListener('keyup', function (event) {
+    var key = event.keyCode || event.charCode || 0;
+    if ([192].indexOf(key) !== -1) {
+      cmdDown = false;
+      document.getElementById('debug').style.visibility = 'hidden';
+    }
   });
 }
